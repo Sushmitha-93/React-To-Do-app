1f13056a25a4e6d9d1f1bf0096ded7e2f1cc4934
Sushmitha DT <dtsushmitha@gmail.com>
2019-04-21 22:30:18 +0530

    Updates

---
+++ precache-manifest.f22611fde3b55e744abdc77a8cdf9cf8.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = [
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/React-To-Do-app/static/media/logo.5d5d9eef.svg"
+  },
+  {
+    "revision": "9cbca47bac9765a9bbf7",
+    "url": "/React-To-Do-app/static/js/runtime~main.425f4cb6.js"
+  },
+  {
+    "revision": "896cbef68704387373a9",
+    "url": "/React-To-Do-app/static/js/main.fd62b236.chunk.js"
+  },
+  {
+    "revision": "1744db918d7799a6e6d6",
+    "url": "/React-To-Do-app/static/js/2.164617b5.chunk.js"
+  },
+  {
+    "revision": "896cbef68704387373a9",
+    "url": "/React-To-Do-app/static/css/main.274b43af.chunk.css"
+  },
+  {
+    "revision": "c1025a5e7a035160276d48b1790dcc2a",
+    "url": "/React-To-Do-app/index.html"
+  }
+];
